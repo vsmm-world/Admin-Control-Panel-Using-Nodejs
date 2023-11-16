7286f81b1ed8ab9d4b91b8751efcefbcfe2b517c
vsmm-world <thekingjarvis792@gmail.com>
2023-11-16 23:14:57 +0530

    some updates

--- models/home.js
+++ models/home.js
@@ -1,5 +1,26 @@
 const mongoose = require('mongoose');
 
-const homeSchem = new mongoose.Schema({
-    
-})
+const homeSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    subtitle:{
+        type:String,
+        required:true,
+    },
+    content:{
+        type:String,
+        required:true,
+    }
+},{
+    timestamps:{
+        createdAt:'created_at',
+        updatedAt:'updated_at',
+    }
+});
+
+const Home = new mongoose.model('HomeTamplate',homeSchema)
+
+module.exports = Home;
+
