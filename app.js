17d6460cd4629ed5473fccf83ac07d25b7cbd7dc
vsmm-world <thekingjarvis792@gmail.com>
2023-11-22 20:59:58 +0530

    small fixing

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 const ConnectDB = require('./db/db');
 const {adminAuth, logout, chekAuth} = require('./auth/auth')
 const cookieParser = require('cookie-parser')
